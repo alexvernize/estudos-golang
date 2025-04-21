65a8b0b4fab1425eab9ced7beeba4d398b549358
Alex Prado <alexlvp@hotmail.com>
2025-04-21 19:56:54 -0300

    doc: Ajustando comentários

--- 20 - Testes pessoais/divisao com erro/main.go
+++ 20 - Testes pessoais/divisao com erro/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 )
 
-// Função simples que divide dois números, não retorna divisão por zero
+// Função simples que recebe e divide dois números. Esta função retorna dois valores,
+// o valor da divisão e nil, caso o divisor seja zero, o número retornado será zero e um erro
+// também é será retornado.
 func divisao(a, b float64) (float64, error) {
 	if b == 0 {
 		return 0, errors.New("Divisão por zero não permitida")
@@ -14,7 +16,7 @@ func divisao(a, b float64) (float64, error) {
 }
 
 func main() {
-	//Cria duas variáveis para a função divide
+	//Cria duas variáveis para a função divisao
 	divisao, erro := divisao(15, 0)
 	if erro != nil {
 		fmt.Println("Erro: ", erro)
